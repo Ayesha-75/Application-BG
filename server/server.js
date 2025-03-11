e18e37566fd0c8c0f424dbb7daac828c95ee1b82
Ayesha-75 <firdousayeshashaik123@gmail.com>
2025-03-11 15:02:48 +0530

    first commit

---
+++ server/server.js
@@ -0,0 +1,21 @@
+import 'dotenv/config'
+import express from 'express'
+import cors from 'cors'
+import connectDB from './configs/mongodb.js'
+// import userRouter from './routes/userRoutes.js'
+
+
+//App config
+const PORT = process.env.PORT || 4000
+const app = express()
+await connectDB()
+
+//Initialize middlewares
+app.use(express.json())
+app.use(cors())   //to connect client 
+
+//API routes
+app.get('/',(req,res)=> res.send("API Working"))
+// app.use('/api/user',userRouter)
+
+app.listen(PORT, ()=> console.log("Server running on port "+PORT))
