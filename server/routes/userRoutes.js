a2529fb2368dcdf8840dfc9e0e1e4422fb34dff9
Ayesha-75 <firdousayeshashaik123@gmail.com>
2025-03-14 00:06:37 +0530

    Completed

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,9 +1,15 @@
 import express from 'express'
-import { clerkWebhooks } from '../controllers/UserController.js'
+import { clerkWebhooks, userCredits } from '../controllers/UserController.js'
+// import { clerkWebhooks, userCredits, paymentRazorpay,verifyRazorpay } from '../controllers/UserController.js'
+
+import authUser from '../middlewares/auth.js'
 
 
 const userRouter = express.Router()
 
 userRouter.post('/webhooks', clerkWebhooks)
+userRouter.get('/credits',authUser,userCredits)
+// userRouter.post('/pay-razor',authUser, paymentRazorpay)
+// userRouter.post('/verify-razor', verifyRazorpay)
 
 export default userRouter
